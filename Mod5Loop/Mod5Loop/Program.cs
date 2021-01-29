63cb1bada152a3f892b7447f909b4297b9755caa
Erick Meraz <emeraz6747@southwestuniversity.edu>
2021-01-29 02:39:32 -0700

    mod 5

---
+++ Mod5Loop/Mod5Loop/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Mod5Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+
+            for (int i=1; i <= 20;i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine("the sum is:" + sum);
+        }
+    }
+}
